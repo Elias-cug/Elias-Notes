fc06fc2937a4404cd87ff98f992e3c7d60272997
liyisheng <liyisheng@bonree.com>
2020-12-18 14:15:38 +0800

    增加prettierrc配置

---
+++ config/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80, // 每行最大字符
+  semi: false, // 是否使用分号
+  singleQuote: true, // 单引号
+  useTabs: false, // 缩进使用Tabs
+  arrowParens: 'avoid',
+  requirePragma: false,
+  proseWrap: 'preserve'
+}
