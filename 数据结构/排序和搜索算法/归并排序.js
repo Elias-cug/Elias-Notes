c0e610abb94c85f980ab10d7c25fdd6e04d24ce5
elias_lee <liyisheng0113@163.com>
2021-04-14 10:01:07 +0800

    增加归并排序

--- 数据结构/排序和搜索算法/归并排序.js
+++ 数据结构/排序和搜索算法/归并排序.js
@@ -1,10 +1,10 @@
 function mergeSort (arr) {
   if (arr.length > 1) {
     const len = arr.length
-    const middle = Math.floor(len / 2)
-    const left = mergeSort(arr.slice(0, middle))
-    const right = mergeSort(arr.slice(middle, len))
-    arr = merge(left, right)
+    const mid = Math.floor(len / 2)
+    const left = mergeSort(arr.slice(0, mid))
+    const right = mergeSort(arr.slice(mid, len))
+    return merge(left, right)
   }
   return arr
 }
@@ -14,9 +14,15 @@ function merge (left, right) {
   let j = 0
   const result = []
   while (i < left.length && j < right.length) {
-    result.push(left[i] < right[j] ? left[i++] : right[j++])
+    if (left[i] < right[j]) {
+      result.push(left[i++])
+    } else {
+      result.push(right[j++])
+    }
   }
-  return result.concat(i < left.length ? left.slice(i) : right.slice(j))
+  return result.concat(
+    i < left.length ? left.slice(i, left.legth) : right.slice(j, right.legth)
+  )
 }
 
-console.log(mergeSort([3, 5, 9, 8, 2, 4, 7]))
+console.log(mergeSort([1, 4, 3, 6, 7, 3, 5, 6, 8, 11, 8, 4]))
