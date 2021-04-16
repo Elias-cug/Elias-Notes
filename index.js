7022ce1253eb1e65ed3f384070a29cb3e9b27145
elias_lee <liyisheng0113@163.com>
2021-04-16 16:52:50 +0800

    前端工程化测试

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-function mergeSort (arr) {
-  if (arr.length > 1) {
-    const len = arr.length
-    const middle = Math.floor(len / 2)
-    const left = mergeSort(arr.slice(0, middle))
-    const right = mergeSort(arr.slice(middle, len))
-    arr = merge(left, right)
-  }
-  return arr
-}
+const { isNil } = require('lodash')
 
-function merge (left, right) {
-  let i = 0
-  let j = 0
-  const result = []
-  while (i < left.length && j < right.length) {
-    result.push(left[i] < right[j] ? left[i++] : right[j++])
-  }
-  return result.concat(i < left.length ? left.slice(i) : right.slice(j))
+if (isNil(null)) {
+  console.log('lll')
 }
-
-console.log(mergeSort([3, 5, 9, 8, 2, 4, 7]))
